123dfebb54db68bce8472a7adc1e4688b572fa62
Patrycja Bilski <patrycja@blsk.pl>
2020-11-13 09:27:56 +0100

    week-four-notes

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const name = 'Patrycja';
 const age = 34;
 
-console.log(`Nazywam się ${name} i mam ${age} lata. Witaj na mojej stronie :)`);
+console.log(
+    `Nazywam się ${name} i mam ${age} lata. Witaj na mojej stronie :)`
+);
+
+
+const heading = document.querySelector('.first-level-header--js');
